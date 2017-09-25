d0ca9f8bcc28f7e4f8248f88a707b8ebb1b58d9a
virtualtech11 <virtualtech11@gmail.com>
2017-09-24 23:56:27 -0600

    add elastic 2.x repo and install elasticsearch

--- provision.sh
+++ provision.sh
@@ -3,3 +3,12 @@ dpkg -s oracle-java8-installer &>/dev/null || {
   echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
   apt-get update && apt-get -y install oracle-java8-installer
 }
+
+dpkg -s elasticsearch &>/dev/null || {
+  wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+  echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-2.x.list
+  apt-get update && sudo apt-get -y install elasticsearch
+  mkdir -p /var/data/elasticsearch
+  chmod 777 /var/data/elasticsearch
+}
+
