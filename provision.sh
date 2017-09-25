9e1b341a49379489c62a7ab3ecca9625cd470051
virtualtech11 <virtualtech11@gmail.com>
2017-09-24 23:45:43 -0600

    add java repository and install it

--- provision.sh
+++ provision.sh
@@ -1,2 +1,5 @@
-apt-get -y update
-apt-get -y dist-upgrade
+dpkg -s oracle-java8-installer &>/dev/null || {
+  add-apt-repository -y ppa:webupd8team/java
+  echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+  apt-get update && apt-get -y install oracle-java8-installer
+}
